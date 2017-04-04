f596dc52b1c372c91827aa9be977b8935406449a
vvtest01 <v-vdesign@v-vdesignui-iMac-2.local>
2017-04-04 15:07:43 +0900

    more design fixes + folder rename

--- incl/theme.js
+++ incl/theme.js
@@ -16,3 +16,16 @@ $(function(){
   $(function(){
     $('#theting').parallaxify(img);
 });
+
+wow = new WOW(
+    {
+    boxClass:     'wow',      // default
+    animateClass: 'animated', // default
+    offset:       0,          // default
+    mobile:       false,       // changed
+    live:         true        // default
+     }
+)
+wow.init();
+
+w3IncludeHTML();
