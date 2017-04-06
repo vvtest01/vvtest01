2e5541f8129deed08868f4a9259f924bcb4bfeec
vvtest01 <v-vdesign@v-vdesignui-iMac-2.local>
2017-04-06 15:14:58 +0900

    change all animations from WOW to regular animate.css

--- incl/js/theme.js
+++ incl/js/theme.js
@@ -1,12 +1,1 @@
-wow = new WOW(
-    {
-    boxClass:     'wow',      // default
-    animateClass: 'animated', // default
-    offset:       0,          // default
-    mobile:       false,       // changed
-    live:         true        // default
-     }
-)
-wow.init();
-
 w3IncludeHTML();
