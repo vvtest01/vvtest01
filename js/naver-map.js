b61a41e33a41552259e3bfc494b804fb78bb0be3
vvtest01 <v-vdesign@v-vdesignui-iMac-2.local>
2017-05-01 14:19:12 +0900

    ?!?!?!?!??!

--- js/naver-map.js
+++ js/naver-map.js
@@ -1,6 +1,5 @@
-var gangnam = new naver.maps.LatLng(37.517744, 127.047369);
-
 //지도 생성시에 옵션을 지정할 수 있습니다.
+var gangnam = new naver.maps.LatLng(37.517744, 127.047369),
 var map = new naver.maps.Map('map', {
         center: new naver.maps.LatLng(37.3595704, 127.105399), //지도의 초기 중심 좌표
         zoom: 2, //지도의 초기 줌 레벨
@@ -9,6 +8,10 @@ var map = new naver.maps.Map('map', {
         zoomControlOptions: { //줌 컨트롤의 옵션
             position: naver.maps.Position.TOP_RIGHT
         }
+    }),
+    marker = new naver.maps.Marker({
+        map: map,
+        position: gangnam
     });
 
 var contentString = [
