5e6d0a9a259a1f2b66a50012a5c35aa74797155a
vvtest01 <v-vdesign@v-vdesignui-iMac-2.local>
2017-05-11 18:13:22 +0900

    SLIDESHOW!

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,26 @@
-$( ".active" ).after( "<hr>" );
+$(function(){
+      $("#slides").slidesjs({
+      	width: 100,
+      	height: 100,
+      	navigation: {
+      		active: false
+      	},
+      	play: {
+      active: true,
+        // [boolean] Generate the play and stop buttons.
+        // You cannot use your own buttons. Sorry.
+      effect: "fade",
+        // [string] Can be either "slide" or "fade".
+      interval: 5000,
+        // [number] Time spent on each slide in milliseconds.
+      auto: false,
+        // [boolean] Start playing the slideshow on load.
+      swap: true,
+        // [boolean] show/hide stop and play buttons
+      pauseOnHover: false,
+        // [boolean] pause a playing slideshow on hover
+      restartDelay: 2500
+        // [number] restart delay on inactive slideshow
+    }
+      });
+    });
