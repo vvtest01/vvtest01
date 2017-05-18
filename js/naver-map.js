61976241779378ee9f1d2e0d661490589eb4e073
vvtest01 <v-vdesign@v-vdesignui-iMac-2.local>
2017-05-18 15:24:15 +0900

    en till

--- js/naver-map.js
+++ js/naver-map.js
@@ -277,6 +277,36 @@ naver.maps.Event.addListener(gangnam, "click", function(e) {
     }
 });
 
+var gangnamdaeroContent = [
+        '<div class="iw_inner">',
+        '   <h3>서울특별시청</h3>',
+        '   <p>서울특별시 중구 태평로1가 31 | 서울특별시 중구 세종대로 110 서울특별시청<br />',
+        '       02-120 | 공공,사회기관 &gt; 특별,광역시청<br />',
+        '       <a href="http://www.seoul.go.kr" target="_blank">www.seoul.go.kr/</a>',
+        '   </p>',
+        '</div>'
+    ].join('');
+
+var infowindow = new naver.maps.InfoWindow({
+    content: gangnamdaeroContent,
+    maxWidth: 140,
+    backgroundColor: "#eee",
+    borderColor: "#2db400",
+    borderWidth: 5,
+    anchorSize: new naver.maps.Size(30, 30),
+    anchorSkew: true,
+    anchorColor: "#eee",
+    pixelOffset: new naver.maps.Point(20, -20)
+});
+
+naver.maps.Event.addListener(gangnamdaero, "click", function(e) {
+    if (infowindow.getMap()) {
+        infowindow.close();
+    } else {
+        infowindow.open(map, gangnamdaero);
+    }
+});
+
 
 // 지도 인터랙션 옵션
 $("#interaction").on("click", function(e) {
