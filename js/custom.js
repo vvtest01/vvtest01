a2f6981bc45ab59b577b70189d36f81d45781e8b
vvtest01 <v-vdesign@v-vdesignui-iMac-2.local>
2017-05-22 17:45:19 +0900

    tings

--- js/custom.js
+++ js/custom.js
@@ -32,3 +32,37 @@ $('.play').on('click', function() {
     $('.slides')
         .slick('slickPlay')
 });
+
+$(window).load(function() {
+  doughnutWidget.options = {
+    container: $('#donut'),
+    width: 100,
+    height: 100,
+    class: 'donut1',
+    cutout: 50
+  };
+
+  doughnutWidget.render(data());
+
+  setInterval(init, 2000);
+});
+
+function init() {
+  doughnutWidget.render(data());
+}
+
+function data() {
+  var data = {
+  서울: {
+    val: Math.round(0.63 * 100),
+    color: '#E24B40'
+  },
+  부산: {
+    val: Math.round(100),
+    color: '#E24B40',
+    class: 'donut2'
+  }
+};
+
+  return data;
+}
