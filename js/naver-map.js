93985ce3c7871212fe610bd803a2636216da62ab
vvtest01 <v-vdesign@v-vdesignui-iMac-2.local>
2017-05-26 17:06:19 +0900

    REVERT

--- js/naver-map.js
+++ js/naver-map.js
@@ -245,36 +245,6 @@ var hwarangro = new naver.maps.Marker({
     map: map
 });
 
-var cityhall = new naver.maps.LatLng(37.5666805, 126.9784147);
-
-var contentString = [
-    '<div class="iw_inner">',
-    '   <h3>서울특별시청</h3>',
-    '   <p>서울특별시 중구 태평로1가 31 | 서울특별시 중구 세종대로 110 서울특별시청<br>',
-    '       <img src="./img/hi-seoul.jpg" width="55" height="55" alt="서울시청" class="thumb" /><br>',
-    '       02-120 | 공공,사회기관 > 특별,광역시청<br>',
-    '       <a href="http://www.seoul.go.kr" target="_blank">www.seoul.go.kr/</a>',
-    '   </p>',
-    '</div>'
-].join('');
-
-var marker = new naver.maps.Marker({
-    map: map,
-    position: cityhall
-});
-
-var infowindow3 = new naver.maps.InfoWindow({
-    content: contentString
-});
-
-naver.maps.Event.addListener(marker, "click", function(e) {
-    if (infowindow3.getMap()) {
-        infowindow3.close();
-    } else {
-        infowindow3.open(map, marker);
-    }
-});
-
 map.setMapTypeId(naver.maps.MapTypeId.HYBRID);
 
 var gangnamContent = [
